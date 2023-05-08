361bec9ba80b0b98b8d95a480145541f58971f88
Saurav Kumar <66282953+sauravsaran99@users.noreply.github.com>
2023-05-08 06:42:52 +0530

    custom user models completed

---
+++ accounts/models.py
@@ -0,0 +1,7 @@
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    name = models.CharField(null=True, blank=True, max_length=100)
